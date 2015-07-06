be48459cec382d8b9b741a92314ccd704e255cf4
John Marshall <john.marshall4997@hotmail.com>
2015-07-06 14:18:13 -0400

    added my first validation to my restaurant form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ActiveRecord::Base
-  # belongs_to :owner, class_name: User
-  # has_many reservations
-  # has_many :users, through: :reservations
+  validates :description, :name, presence: true
+  validates :capacity, numericality: {only_integer: true}
 end
 
